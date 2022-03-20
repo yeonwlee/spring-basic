1d028c60f7bb6bb15fd7b6c568439bc80cab7bfe
Yeonwoo Lee <tlstprl1806@naver.com>
2022-03-20 11:45:27 +0900

    basic spring

---
+++ src/test/java/hello/core/OrderAppTest.java
@@ -0,0 +1,7 @@
+package hello.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderAppTest {
+
+}
---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.order;
+
+import hello.core.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    void beforEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
+    @Test
+    void 주문생성_할인가격() {
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
